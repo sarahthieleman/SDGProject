54c9099bd6e6306d23e72877c3b8143fcecaafdf
Sarah Thieleman <thie0014@hz.nl>
2019-06-19 14:38:07 +0200

    layout page

---
+++ resources/views/opleidingen.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Page Title')
+
+@section('header')
+    <p>This is my header content.</p>
+@endsection
+
+@section('navbar')
+    <p>This is my navbar content.</p>
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
+
+@section('sidebar')
+    <p>This is my sidebar content.</p>
+@endsection
+
+@section('footer')
+    <p>This is my footer content.</p>
+@endsection
